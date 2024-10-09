70612520e05739e676db309912b497d2d397c364
Aditya Saha <aadi@Adityas-MacBook-Air.local>
2024-10-09 23:37:10 +0530

    first commit

---
+++ src/main/java/com/foodorder/userinfo/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.foodorder.userinfo.controller;
+
+import com.foodorder.userinfo.dto.UserDTO;
+import com.foodorder.userinfo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/addUser")
+    public ResponseEntity<UserDTO> addUser(@RequestBody UserDTO userDTO) {
+        UserDTO savedUser = userService.addUser(userDTO);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/fetchUserById/{userId}")
+    public ResponseEntity<UserDTO> fetchUserDetailsById(@PathVariable Integer userId) {
+        return userService.fetchUserDetailsById(userId);
+    }
+}
